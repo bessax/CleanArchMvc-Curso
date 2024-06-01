27bfd398dd055e0032922218cdb5bd85c4bf4e42
bessax <andrebessax@gmail.com>
2024-06-01 11:53:10 -0300

    #update: Aula 29 - Enriquecendo o modelo de domínio Category.

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,7 +1,33 @@
-﻿namespace CleanArchMvc.Domain.Entities;
-public class Category
+﻿
+using CleanArchMvc.Domain.Validation;
+
+namespace CleanArchMvc.Domain.Entities;
+public sealed class Category
 {
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public ICollection<Product> Products { get; set; }
+    public int Id { get; private set; }
+    public string? Name { get; private set; }
+    public ICollection<Product>? Products { get; set; }
+
+    public Category(string? name)
+    {      
+        ValidationDomain(name);   
+    }
+
+    public Category(int id, string? name)
+    {
+        DomainExceptionValidation.When(id < 0, "Invalid Id value");
+        Id = id;
+        ValidationDomain(name);
+    }
+
+    private void ValidationDomain(string? name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
+
+        DomainExceptionValidation.When(name!.Length < 3, "Invalid name. Too short, minimum 3 characters");
+
+        DomainExceptionValidation.When(name!.Length > 100, "Invalid name. Too long, maximum 100 characters");
+
+        this.Name = name;
+    }
 }
