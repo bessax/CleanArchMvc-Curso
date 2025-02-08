1e8576c38994dd1d2dac770df1c83a43e3ce713e
bessax <andrebessax@gmail.com>
2025-02-08 20:02:22 -0300

    #update: registros das dependências na Infra.IOC

--- CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,10 +1,13 @@
 ﻿using CleanArchMvc.Application.Interfaces;
 using CleanArchMvc.Application.Mappings;
 using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
 using CleanArchMvc.Infra.Data.Repositories;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,19 +19,34 @@ public static class DependencyInjection
     {
         services.AddDbContext<ApplicationDbContext>(options =>
                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),b=>b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
-        
+
+        services.AddIdentity<ApplicationUser, IdentityRole>(/*options => options.SignIn.RequireConfirmedAccount = true*/)
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        //Essa configuração redireciona o usuário para a página de login quando ele tenta acessar uma página que não tem permissão
+        services.ConfigureApplicationCookie(options =>
+        {
+            options.AccessDeniedPath = "/Account/Login";
+        });
+
         services.AddScoped<ICategoryRepository, CategoryRepository>();
         services.AddScoped<IProductRepository, ProductRepository>();
 
         services.AddScoped<IProductService, ProductService>();
         services.AddScoped<ICategoryService, CategoryService>();
 
+        services.AddScoped<IAuthenticate, AuthenticateService>();
+        services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
+        
+
         services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
         var myHandler = AppDomain.CurrentDomain.GetAssemblies().Where(a => a.FullName.Contains("CleanArchMvc.Application")).FirstOrDefault();
 
         services.AddMediatR(myHandler);
-
+        
         return services;
     }
 }
