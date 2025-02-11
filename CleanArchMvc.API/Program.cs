a59e06ffd848d95e94e9b3b8cd6f5dac1c62c25e
bessax <andrebessax@gmail.com>
2025-02-11 17:50:31 -0300

    #update: implementação da camada de API ao projeto: - Adicionada autenticação e autorização com token JWT; - Implementação de classe de extensão para melhorar a manutenibilidade do código;

---
+++ CleanArchMvc.API/Program.cs
@@ -0,0 +1,63 @@
+using CleanArchMvc.Infra.IoC;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddInfrastructureAPI(builder.Configuration);
+//ativar autenticacao e validar o token
+builder.Services.AddInfrastructureJWT(builder.Configuration);
+builder.Services.AddInfrastructureSwagger();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v2", new OpenApiInfo
+    {
+        Version = "v2",
+        Title = "CleanArchMvc API",
+        Description = "Projeto API",
+        TermsOfService = new Uri("https://examplp.com/termoservico"),
+        Contact = new OpenApiContact
+        {
+            Name = "Contato",
+            Url = new Uri("https://examplo.com/contato")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Licença",
+            Url = new Uri("https://examplo.com/licenca")
+        }
+    });
+
+    // usando System.Reflection;
+    //var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    //options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+// Ensure the extension method AddInfrastructureAPI is available
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStatusCodePages();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
---
+++ CleanArchMvc.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,44 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
+using CleanArchMvc.Infra.Data.Repositories;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchMvc.Infra.IoC;
+public static class DependencyInjectionAPI
+{
+    public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+                  options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>(/*options => options.SignIn.RequireConfirmedAccount = true*/)
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<ICategoryService, CategoryService>();
+
+        services.AddScoped<IAuthenticate, AuthenticateService>();
+        //services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
+        services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+        var myHandler = AppDomain.CurrentDomain.GetAssemblies().Where(a => a.FullName.Contains("CleanArchMvc.Application")).FirstOrDefault();
+
+        services.AddMediatR(myHandler);
+
+        return services;
+    }
+}
